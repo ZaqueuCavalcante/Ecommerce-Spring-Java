e42218fe10a2e0698f52e631ab7b2a7abd4d1265
Zaqueu C <zaqueudovale@gmail.com>
2020-08-20 20:28:50 -0300

    First commit.

---
+++ src/main/java/br/com/zaqueucavalcante/ecommercespringjava/EcommerceSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zaqueucavalcante.ecommercespringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringJavaApplication.class, args);
+	}
+
+}
